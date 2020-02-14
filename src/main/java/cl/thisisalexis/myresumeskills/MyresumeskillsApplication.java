1acd92399cd96e09d353bc2e28ef6ec075ebf893
Alexis <alexis.ve@gmail.com>
2020-02-14 16:09:55 -0300

    chor: first commit. Spring Boot app added to repo

---
+++ src/main/java/cl/thisisalexis/myresumeskills/MyresumeskillsApplication.java
@@ -0,0 +1,13 @@
+package cl.thisisalexis.myresumeskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyresumeskillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyresumeskillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/thisisalexis/myresumeskills/MyresumeskillsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.thisisalexis.myresumeskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyresumeskillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
