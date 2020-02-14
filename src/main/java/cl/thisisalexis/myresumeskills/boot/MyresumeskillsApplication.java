b87888555cadd1559f7bd1ea01d6a82894d121ba
Alexis <alexis.ve@gmail.com>
2020-02-14 16:20:06 -0300

    chor: basic configuration for app has been set.

--- src/main/java/cl/thisisalexis/myresumeskills/boot/MyresumeskillsApplication.java
+++ src/main/java/cl/thisisalexis/myresumeskills/boot/MyresumeskillsApplication.java
@@ -1,9 +1,9 @@
-package cl.thisisalexis.myresumeskills;
+package cl.thisisalexis.myresumeskills.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "cl.thisisalexis.myresumeskills")
 public class MyresumeskillsApplication {
 
 	public static void main(String[] args) {
